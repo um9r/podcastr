783442482597531efe7795b2ff6259b8541acbca
Umar <faroukumar99@gmail.com>
2025-03-03 15:53:36 +0100

    Implement Convex & Clerk Setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://dashing-gnu-72.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
